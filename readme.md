401b6e5082a10a76d2f895149da1c5d88ca197d4
Todd Sifleet <todd.sifleet@gmail.com>
2012-12-24 20:31:13 -0500

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,43 @@
+Python RegEx
+=============
+
+A simple wrapper around python's [re module](http://docs.python.org/2/library/re.html), that makes it a little easier to work with.  I really like the way perl handles regex so this wraps up something similar.
+
+Usage
+-------
+    from regex import RegEx
+    regex = RegEx('^(?P<this>this) .* (?P<test>test)$')
+
+    if regex('this is a test'):
+        print "Passes as expected"
+        print "Found: %s" % regex.test
+
+
+Why?
+-------
+
+So that you don't have to do:
+
+    x = re.search('^(?P<this>this) .* (?P<test>test)$')
+    if x is not None:
+        print x.group('this')
+
+So that you can:
+
+-access results like a list, dictionary, or attribute
+
+-get a default value back, on a missed match
+
+-stop testing first, just grab the reuslt
+
+
+Overall if you like working with regex in perl, this makes it easier in python.
+
+License:
+-------
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ regex.py
@@ -0,0 +1,58 @@
+import re
+
+class RegEx(object):
+    default = None
+    regex = None
+    results = None
+    def __init__(self, regex = None, flags = 0):
+        if regex is not None:
+            self.regex = re.compile(regex, flags)
+
+    def __call__(self, string, regex = None, flags = 0):
+        if regex is not None:
+            self.regex = re.compile(regex, flags)
+        if self.regex is None:
+            raise Exception("You must define a regex before or during your call")
+
+        self.results = re.search(self.regex, string)
+        return self
+
+    def __getitem__(self, keys):
+        keys = keys if isinstance(keys, tuple) else (keys, )
+        return self._get_group_item(keys)
+
+    def __getattr__(self, name):
+        return self._get_group_item((name,))
+
+    def _get_group_item(self, keys):
+        #if we don't have any info we don't have to bother looking
+        if not self.results:
+            return self.default
+
+        try:
+            return self.results.group(*keys)
+        except:
+            return self.default
+
+    def __nonzero__(self):
+        return self.results is not None
+
+if __name__ == '__main__':
+    #create an instance with a regex string
+    regex = RegEx('^(?P<this>this) .* (?P<test>test)$')
+
+    if regex('this is a test'):
+        print "regex('this is a test'):\n\t Passes as expected"
+        print "Access like a dictionary (regex['test']): \n\t%s" % regex['test']                
+        print "Get more than one value (regex['test', 'this']): \n\t%s" % ', '.join(regex['test', 'this'])
+        print "Access as an attribute (regex.test): \n\t%s" % regex.test
+        print "Access as an index (regex[1]): \n\t%s" % regex[0]
+        print "doesn't die, returns None (regex.does_not_exist):\n\t %s" % str(regex.does_not_exist)
+
+    print "\nYou can even do it all in one go:"
+    print "RegEx('^(?P<this>this) .* (?P<test>test)$')('this is a test').this: \n\t%s" % RegEx('^(?P<this>this) .* (?P<test>test)$')('this is a test').this
+    print "regex('this is a test').this: \n\t%s" % regex('this is a test').this
+
+    if not regex('this is test that will fail!'):
+        print "\nregex('this is test that will fail!'):\n\tFailed as expected"
+        print "Old results are cleared out (regex['test']): \n\t%s" % regex['test']
